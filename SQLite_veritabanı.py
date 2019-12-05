175626ff462f6e4c6b5146d97c93037eabc89fd5
DarthApex <39526242+alper7737@users.noreply.github.com>
2019-12-05 12:13:56 +0300

    Delete SQLite_veritabanı.py

--- SQLite_veritabanı.py
+++
@@ -1,9 +0,0 @@
-import sqlite3 as sql
-
-veriad= sql.connect('kisiler.sqlite')
-
-imlec = veriad.cursor()
-imlec.execute("CREATE TABLE kisi_bilgisi (adi,soyadi,telefon)")
-kisi_girisi = "INSERT INTO kisi_bilgisi VALUES ('Alper','Coşar','05307035076')"
-imlec.execute(kisi_girisi)
-veriad.commit()
